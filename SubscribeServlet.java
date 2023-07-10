0defab15b60c3887317f477a6ec2539b5c1afd79
daniloreply <139113807+daniloreply@users.noreply.github.com>
2023-07-10 16:41:03 +0200

    Fix 4 - user enumeration
    Eliminata il doppio messaggio in base ad email registrata. Tolta visualizzazione dell'email nella response

---
+++ SubscribeServlet.java
@@ -0,0 +1,44 @@
+package com.notsecurebank.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import com.notsecurebank.util.DBUtil;
+import com.notsecurebank.util.ServletUtil;
+
+public class SubscribeServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOG = LogManager.getLogger(SubscribeServlet.class);
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.info("doPost");
+
+        String email = request.getParameter("txtEmail");
+        if (email == null || !ServletUtil.isValidEmail(email)) {
+            LOG.error("Invalid e-mail: '" + email + "'.");
+            response.sendRedirect("index.jsp");
+            return;
+        }
+
+        String messageSubscribe = null;
+        try {
+            DBUtil.addSubscription(email);
+            messageSubscribe = "Thank you for the subscribe" + "Please <a href='locations.jsp'>search</a> for the Branch Office closest to you and ask them for an account.";
+        } catch (Exception e) {
+            messageSubscribe = "Unexpected error.";
+        }
+
+        request.setAttribute("message_subscribe", messageSubscribe);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("subscribe.jsp");
+        dispatcher.forward(request, response);
+
+    }
+}
